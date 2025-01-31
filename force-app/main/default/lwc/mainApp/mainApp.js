ea984d7b9caf3e33ffd2937b3c4141a50985476a
Jacob Tep <jacobptep@gmail.com>
2025-01-31 13:35:00 -0800

    initial commit

---
+++ force-app/main/default/lwc/mainApp/mainApp.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+import COMPANY_LOGO from '@salesforce/resourceUrl/Logo'
+export default class MainApp extends LightningElement {
+    logoUrl = COMPANY_LOGO;
+}
